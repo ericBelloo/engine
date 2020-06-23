302f69f889191fe5bfceb3efde4f1a8d32dbacd5
eric bello <ericbello@outlook.com>
2020-06-23 15:12:56 -0500

    login

--- apps/person/urls.py
+++ apps/person/urls.py
@@ -5,6 +5,7 @@ from apps.person.views import *
 app_name = 'person'
 
 urlpatterns = [
+    path('', PersonLoginView.as_view(), name='login'),
     path('login/', PersonLoginView.as_view(), name='login'),
     path('sign_up/', PersonSignUpView.as_view(), name='sign_up'),
     path('logout/', logout_view, name='logout'),
